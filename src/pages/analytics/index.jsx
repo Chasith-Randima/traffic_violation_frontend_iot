57dbe6a5eb1e665d59a2445fcd2f0dec425365e8
Chasith-Randima <chasith19@gmail.com>
2023-04-20 18:41:52 +0530

    created analytics page for all the police stations.

--- src/pages/analytics/index.jsx
+++ src/pages/analytics/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Layout from "components/Layout";
 import CasesByCity from "components/analytics/CasesByCity";
 import CasesCountByActive from "components/analytics/CasesCountByActive";
